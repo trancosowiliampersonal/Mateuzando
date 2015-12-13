b313e6a83764ec1de0921c6aca1134c52d69bd9d
wiliamtrancoso <wileleh@gmail.com>
2015-12-13 17:30:16 -0200

    [master] [wiliam trancoso]   \_> versionando o projeto ja startado

---
+++ app/src/main/java/br/com/dalcim/mateuzando/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package br.com.dalcim.mateuzando.activity;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import br.com.dalcim.mateuzando.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
+    }
+
+}
